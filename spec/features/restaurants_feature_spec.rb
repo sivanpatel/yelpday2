719bc49a727f0f4d05b2ed336d9443b13f3ec75f
Sivan Patel <sivan.patel@googlemail.com>
2015-09-02 13:34:44 +0100

    Added tests & implementation for deleting restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -58,4 +58,16 @@ feature 'restaurants' do
     end
   end
 
+  context 'deleting restaurants' do
+
+    before {Restaurant.create name: 'TFC'}
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete TFC'
+      expect(page).not_to have_content 'TFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
+
 end
