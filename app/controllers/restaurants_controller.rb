7a2dadfa6b33b43f1f1deb0f9168b0a367ce7168
Sivan Patel <sivan.patel@googlemail.com>
2015-09-04 15:05:05 +0100

    Removing merge conflicts

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,6 +11,7 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
     if @restaurant.save
       redirect_to '/restaurants'
     else
